4d72cb9a6d382e4d037a3462d68bde609712ec75
rpserjo <rpserjo@raspberry.pi>
2023-03-23 13:48:53 +0000

    sprint_2

--- src/utils/HTTPTransport.ts
+++ src/utils/HTTPTransport.ts
@@ -16,29 +16,29 @@ interface Options {
 class HTTPTransport {
     private queryString = (data: Record<string, string>) => {
         return `?${Object.entries(data).map((param) => `${param[0]}=${param[1]}`).join('&')}`;
-    }
+    };
 
     protected get = (url: string, options: Options = {}): Promise<XMLHttpRequest> => {
         const { data } = options;
         url = `${url}${this.queryString(data)}`;
         return this.request(url, { ...options, method: Methods.GET }, options.timeout);
-    }
+    };
 
     protected post = (url: string, options: Options = {}): Promise<XMLHttpRequest> => {
         return this.request(url, { ...options, method: Methods.POST }, options.timeout);
-    }
+    };
 
     protected put = (url: string, options: Options = {}): Promise<XMLHttpRequest> => {
         return this.request(url, { ...options, method: Methods.PUT }, options.timeout);
-    }
+    };
 
     protected patch = (url: string, options: Options = {}): Promise<XMLHttpRequest> => {
         return this.request(url, { ...options, method: Methods.PATCH }, options.timeout);
-    }
+    };
 
     protected delete = (url: string, options: Options = {}): Promise<XMLHttpRequest> => {
         return this.request(url, { ...options, method: Methods.DELETE }, options.timeout);
-    }
+    };
 
     private request = (url: string, options: Options, timeout = 5000): Promise<XMLHttpRequest> => {
         const { method, data, headers = {} } = options;
@@ -62,11 +62,11 @@ class HTTPTransport {
 
             if(method === Methods.GET) {
                 xhr.send();
-            }else{
+            } else {
                 xhr.send(data);
             }
         });
-    }
+    };
 }
 
 export default HTTPTransport;
